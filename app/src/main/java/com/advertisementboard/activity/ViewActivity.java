c542209919668ce9127553cc164b1c5bf85baa10
Maxim <maksimka.fetisov.02@mail.ru>
2024-01-07 19:14:42 +0300

    Подтверждение и отклонение

--- app/src/main/java/com/advertisementboard/activity/ViewActivity.java
+++ app/src/main/java/com/advertisementboard/activity/ViewActivity.java
@@ -3,13 +3,26 @@ package com.advertisementboard.activity;
 import static java.util.Objects.nonNull;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import com.advertisementboard.R;
+import com.advertisementboard.config.AppConfiguration;
 import com.advertisementboard.data.dto.advertisement.AdvertisementDto;
+import com.advertisementboard.data.dto.user.UserDto;
+import com.advertisementboard.data.enumeration.AdvertisementStatus;
+import com.advertisementboard.data.enumeration.UserRole;
 import com.advertisementboard.databinding.ActivityViewBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ViewActivity extends AppCompatActivity {
 
@@ -22,6 +35,12 @@ public class ViewActivity extends AppCompatActivity {
     private TextView urlLabelTextView;
 
     private TextView contactsLabelTextView;
+
+    private Button confirmButton;
+
+    private Button rejectButton;
+
+    private CoordinatorLayout coordinatorLayout;
 
 
     private AdvertisementDto advertisement;
@@ -37,7 +56,12 @@ public class ViewActivity extends AppCompatActivity {
         descriptionLabelTextView = findViewById(R.id.descriptionLabelTextView);
         urlLabelTextView = findViewById(R.id.urlLabelTextView);
         contactsLabelTextView = findViewById(R.id.contactsLabelTextView);
+        confirmButton = findViewById(R.id.confirmButton);
+        rejectButton = findViewById(R.id.rejectButton);
+        coordinatorLayout = findViewById(R.id.viewCoordinatorLayout);
 
+        confirmButton.setOnClickListener(view -> confirm());
+        rejectButton.setOnClickListener(view -> reject());
 
         if(nonNull(getIntent().getExtras())
                 && getIntent().getExtras().containsKey("advertisement")) {
@@ -54,6 +78,75 @@ public class ViewActivity extends AppCompatActivity {
             }
         }
 
+        processButtons();
+    }
+
+    private void processButtons() {
+        UserDto user = AppConfiguration.user();
+        boolean isModerator = nonNull(user.getRole())
+                && nonNull(user.getRole().getName())
+                && (user.getRole().getName().equals(UserRole.MODERATOR.name())
+        || user.getRole().getName().equals(UserRole.ADMINISTRATOR.name()));
+
+        confirmButton.setVisibility(isModerator
+                && advertisement.getStatus() != AdvertisementStatus.CONFIRMED
+                ? View.VISIBLE
+                : View.INVISIBLE);
+        rejectButton.setVisibility(isModerator
+                && advertisement.getStatus() != AdvertisementStatus.REJECTED
+                ? View.VISIBLE
+                : View.INVISIBLE);
+
+    }
+
+    private void confirm() {
+        AppConfiguration.advertisementClient().confirmAdvertisement(advertisement.getId()).enqueue(
+                new Callback<>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        if (response.code() == 200) {
+                            Log.i("Advertisement", "Advertisement confirmed");
+                            Snackbar.make(coordinatorLayout, R.string.confirmed, Snackbar.LENGTH_SHORT).show();
+                            advertisement.setStatus(AdvertisementStatus.CONFIRMED);
+                            processButtons();
+                        } else {
+                            Log.e("Advertisement", "Error confirming the advertisement: " + response.code());
+                            Snackbar.make(coordinatorLayout, R.string.confirm_error, Snackbar.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        Log.e("Advertisement", "No connection");
+                        Snackbar.make(coordinatorLayout, R.string.no_connection, Snackbar.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+
+    private void reject() {
+        AppConfiguration.advertisementClient().rejectAdvertisement(advertisement.getId()).enqueue(
+                new Callback<>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        if (response.code() == 200) {
+                            Log.i("Advertisement", "Advertisement rejected");
+                            Snackbar.make(coordinatorLayout, R.string.rejected, Snackbar.LENGTH_SHORT).show();
+                            advertisement.setStatus(AdvertisementStatus.REJECTED);
+                            processButtons();
+                        } else {
+                            Log.e("Advertisement", "Error rejecting the advertisement: " + response.code());
+                            Snackbar.make(coordinatorLayout, R.string.reject_error, Snackbar.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        Log.e("Advertisement", "No connection");
+                        Snackbar.make(coordinatorLayout, R.string.no_connection, Snackbar.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 
 }
